b240bc547d5f7645a2961c830d1bdee8b544327c
csfwff <csfwff@163.com>
2021-10-29 17:08:00 +0800

    :tada: add react antd

--- main.js
+++ main.js
@@ -1,28 +1,71 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
-const drag = require('electron-drag')
-
-const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600,
-      opacity :0.5,
-      alwaysOnTop :true,
-      webPreferences: {
-        preload: path.join(__dirname, 'preload.js')
-      }
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+let mainWindow = null;
+//判断命令行脚本的第二参数是否含--debug
+const debug = /--debug/.test(process.argv[2]);
+function makeSingleInstance () {
+    if (process.mas) return;
+    app.requestSingleInstanceLock();
+    app.on('second-instance', () => {
+        if (mainWindow) {
+            if (mainWindow.isMinimized()) mainWindow.restore()
+            mainWindow.focus()
+        }
     })
-  
-    win.loadFile('index.html')
-  }
+}
+function createWindow () {
+    const windowOptions = {
+        width: 400,
+        height: 300,
+        opacity :0.5,
+        // frame:false,
+        alwaysOnTop:true,
+    };
+    mainWindow = new BrowserWindow(windowOptions);
+    mainWindow.loadURL("http://localhost:3000/");
+    // mainWindow.loadURL("http://pwl.icu/");
+    // mainWindow.loadURL(path.join('file://', __dirname, '/build/index.html'));
+    //接收渲染进程的信息
+    const ipc = require('electron').ipcMain;
+    ipc.on('min', function () {
+        mainWindow.minimize();
+    });
+    ipc.on('max', function () {
+        mainWindow.maximize();
+    });
+    ipc.on("login",function () {
+        mainWindow.maximize();
+    });
+    //如果是--debug 打开开发者工具，窗口最大化，
+    if (debug) {
+        // mainWindow.webContents.openDevTools();
+    }
 
-  app.whenReady().then(() => {
-    createWindow()
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-      })
-  })
+    mainWindow.on("blur",(e)=>{
+      mainWindow.setOpacity(0.5)
+    })
 
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-  })
+    mainWindow.on("focus",()=>{
+      mainWindow.setOpacity(1)
+    })
+
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    })
+}
+makeSingleInstance();
+//app主进程的事件和方法
+app.on('ready', () => {
+    createWindow();
+});
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+module.exports = mainWindow;
